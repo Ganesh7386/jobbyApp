42d1c15a0284fa1a21a6c4d91ffbbdcbde03a479
Ganesh7386 <puttaganesh7386@gmail.com>
2024-04-25 12:05:18 +0000

    removed extra space error in home route

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,28 @@
+import {Component} from 'react'
+import {Link} from 'react-router-dom'
 import './index.css'
 
-function Home() {
-  return (
-    <div className="HomeContainer">
-      <h1>This is home </h1>
-      <h1>Find Jobs</h1>
+class Home extends Component {
+  displayOverAllUi = () => (
+    <div className="homeStyling">
+      <h1>
+        Find The Job That <br /> Fits Your Life
+      </h1>
+      <p>
+        Find
+        uhdsknajhucnkjnkjncjknkjnakjkjankjnajkdnkjhiueaknkdjandjknkjdbjkbkjj
+      </p>
+      <Link to="/jobs">
+        <button className="homeBuuttonStyling" type="button">
+          jobs
+        </button>
+      </Link>
     </div>
   )
+
+  render() {
+    return this.displayOverAllUi()
+  }
 }
 
 export default Home
